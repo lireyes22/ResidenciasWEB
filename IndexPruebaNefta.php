0ffc05229f338cf9b94157aa89c426468d344acc
Nefta56 <128199555+Nefta56@users.noreply.github.com>
2023-04-22 23:44:54 -0500

    j

---
+++ IndexPruebaNefta.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Título de la página</title>
+</head>
+<body>
+	<header>
+		<nav>
+			<ul>
+				<li><a href="#">Inicio</a></li>
+				<li><a href="#">Acerca de</a></li>
+				<li><a href="#">Servicios</a></li>
+				<li><a href="#">Productos</a></li>
+				<li><a href="#">Contacto</a></li>
+			</ul>
+		</nav>
+	</header>
+	<main>
+		<!-- Contenido de la página -->
+	</main>
+	<footer>
+		<!-- Pie de página -->
+	</footer>
+</body>
+</html>
