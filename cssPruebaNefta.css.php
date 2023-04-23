174b144875f7eadab7b8c7bef6d0f47815727399
Nefta56 <128199555+Nefta56@users.noreply.github.com>
2023-04-22 23:46:25 -0500

    Update cssPruebaNefta.css.php

---
+++ cssPruebaNefta.css.php
@@ -0,0 +1,27 @@
+nav {
+  background-color: #333;
+  color: #fff;
+  height: 50px;
+}
+
+nav ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+}
+
+nav li {
+  margin-right: 20px;
+}
+
+nav a {
+  color: #fff;
+  text-decoration: none;
+  padding: 15px;
+  display: block;
+}
+
+nav a:hover {
+  background-color: #555;
+}
